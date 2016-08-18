4214f5692321842d393e30608889c2166240195d
Gustavo Sobral <ghsobral@gmail.com>
2016-08-18 01:42:10 -0300

    Configure devise paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    admin_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   scope '/admin' do
-    devise_for :users
+    devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   end
-  
+
   namespace :admin do
     root to: 'home#index'
   end  
