83c1039be4750a5d05c86db03e075c01c3fbb1c8
Gustavo Sobral <ghsobral@gmail.com>
2016-09-03 10:32:53 -0300

    Rename project

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_lift_ads_session'
+Rails.application.config.session_store :cookie_store, key: '_myscreen_session'
