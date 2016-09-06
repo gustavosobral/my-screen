c2b977fed669bdbbd45e8893fa1c7b615ac541d4
Gustavo Sobral <ghsobral@gmail.com>
2016-09-06 16:57:05 -0300

    Validates Image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,13 @@
 class Image < Resource
   mount_uploader :file, ImageUploader
+  validate :image_size
+
+  private
+
+    # Validates the size of an uploaded image.
+    def image_size
+      if file.size > 4.megabytes
+        errors.add(:file, 'deve ser menor que 4MB')
+      end
+    end
 end
