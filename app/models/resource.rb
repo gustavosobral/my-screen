a50b7c9b69680fff0f22821e6267710bbffebb3c
Gustavo Sobral <ghsobral@gmail.com>
2016-09-06 16:16:41 -0300

    Increase Resource description size limit to 255

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,5 @@
 class Resource < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 140 }
+  validates :description, presence: true, length: { maximum: 255 }
   validates_presence_of :file
 end
