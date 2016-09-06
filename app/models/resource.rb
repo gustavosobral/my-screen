374523d85bb0ebe84cd2d9b82eb9574480f3e0f2
Gustavo Sobral <ghsobral@gmail.com>
2016-09-06 15:48:14 -0300

    Show image.errors and highlight field_with_errors

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,5 @@
 class Resource < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 140 }
+  validates_presence_of :file
 end
