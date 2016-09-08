ce4c042e6707a39a8684f5b27d7ee05056ceedf8
Gustavo Sobral <ghsobral@gmail.com>
2016-09-08 18:53:13 -0300

    Add Video specs

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -4,4 +4,12 @@ FactoryGirl.define do
     title         Faker::Lorem.characters(20)
     description   Faker::Lorem.characters(120)
   end
+
+  factory :invalid_video, parent: :video do
+    file          { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'SampleVideo_1280x720_1mb_invalid.mp4')) }
+  end
+
+  factory :invalid_video_format, parent: :video do
+    file          { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'SampleVideo_1280x720_1mb.mkv')) }
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,4 +3,14 @@ describe Video do
     video = FactoryGirl.build(:video)
     expect(video.valid?).to be true
   end
+
+  it 'should raise error on invalid video' do
+    expect { FactoryGirl.build(:invalid_video) }.to raise_error(FFMPEG::Error)
+  end
+
+  it 'should not accept invalid video formats' do
+    video = FactoryGirl.build(:invalid_video_format)
+    expect(video.valid?).to be false
+    expect(video.errors.messages[:file].join).to include("não é permitido o envio de arquivos \"mkv\"")
+  end
 end
