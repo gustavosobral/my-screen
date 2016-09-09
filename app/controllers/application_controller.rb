99e53fe68dd9a8246196495cde0c1911ea426829
Gustavo Sobral <ghsobral@gmail.com>
2016-09-09 01:28:19 -0300

    Add a Home application static page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def after_sign_in_path_for(_)
+      admin_root_path
+    end
+
     def after_sign_out_path_for(_)
       root_path
     end
