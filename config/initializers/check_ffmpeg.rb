248df59070b8ada005e35fcc4f36f384c2e4c255
Gustavo Sobral <ghsobral@gmail.com>
2016-09-10 15:49:02 -0300

    Disable FFmpeg check on Heroku environment

--- config/initializers/check_ffmpeg.rb
+++ config/initializers/check_ffmpeg.rb
@@ -1,2 +1,4 @@
-`which ffmpeg`.strip
-raise SystemCallError, 'FFmpeg not found. You need to have it to generate thumbnails from videos' unless $?.success?
+unless ENV.fetch('HEROKU_ENV', false)
+  `which ffmpeg`.strip
+  raise SystemCallError, 'FFmpeg not found. You need to have it to generate thumbnails from videos' unless $?.success?
+end
