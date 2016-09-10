919daa94e875f115ec5a7cfdf716d73185bbe512
Gustavo Sobral <ghsobral@gmail.com>
2016-09-10 17:04:13 -0300

    Add back button and style improvments to forms

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,8 @@ if Rails.env.test? || Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = true
-    FFMPEG.logger.level = 5
+
+    # Disable FFMPEG logger
+    FFMPEG.logger = Logger.new(nil)
   end
 end
