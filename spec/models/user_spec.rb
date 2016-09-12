8e7d2973145dad5268c4b87f6eeb5fed4437772f
Gustavo Sobral <ghsobral@gmail.com>
2016-09-12 17:25:04 -0300

    Add spec for User email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,12 @@ describe User do
     user = FactoryGirl.build(:user)
     expect(user.valid?).to be true
   end
+
+  it 'email should be unique' do
+    user = FactoryGirl.build(:user)
+    user.save
+    other_user = FactoryGirl.build(:user, name: 'Jose')
+    expect(other_user.valid?).to be false
+    expect(other_user.errors.messages[:email].join).to match('já está em uso')
+  end
 end
