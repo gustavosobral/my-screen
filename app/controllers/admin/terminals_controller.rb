b8fb5e1c0676b1179aa00f8d401831f8232e3705
Gustavo Sobral <ghsobral@gmail.com>
2016-09-18 17:34:02 -0300

    Add terminal CRUD for admin users

---
+++ app/controllers/admin/terminals_controller.rb
@@ -0,0 +1,58 @@
+class Admin::TerminalsController < Admin::ApplicationController
+  add_breadcrumb 'Terminais', :admin_terminals_path
+
+  def index
+    @terminals = Terminal.page(params[:page])
+  end
+
+  def new
+    add_breadcrumb 'Novo'
+    @terminal = Terminal.new
+  end
+
+  def edit
+    add_breadcrumb 'Editar'
+    @terminal = set_terminal
+  end
+
+  def create
+    add_breadcrumb 'Novo'
+    @user = User.find(params[:terminal][:user_id])
+    @terminal = @user.terminals.new(terminals_params)
+
+    if @terminal.save
+      flash[:notice] = 'Terminal salvo com sucesso!'
+      redirect_to admin_terminals_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    add_breadcrumb 'Editar'
+    @terminal = set_terminal
+    if @terminal.update(terminals_params)
+      flash[:notice] = 'Terminal alterado com sucesso!'
+      redirect_to admin_terminals_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @terminal = set_terminal
+    @terminal.destroy
+    flash[:notice] = 'Terminal excluído com sucesso!'
+    redirect_to admin_terminals_path
+  end
+
+  private
+
+    def set_terminal
+      Terminal.find(params[:id])
+    end
+
+    def terminals_params
+      params.require(:terminal).permit(:user_id, :title)
+    end
+end
