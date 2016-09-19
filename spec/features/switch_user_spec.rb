79324959b43e3c51144149df027108b758c58db3
Gustavo Sobral <ghsobral@gmail.com>
2016-09-19 17:35:34 -0300

    Fix switch user spec

--- spec/features/switch_user_spec.rb
+++ spec/features/switch_user_spec.rb
@@ -7,14 +7,12 @@ feature 'I switch user', type: :feature do
     visit admin_users_path
 
     # Switch user
-    expect(page).to have_link(nil, href: switch_user_path(user))
-    click_link_by_href switch_user_path(user)
+    find("form[action='#{switch_user_path(user)}']").click_button
     expect(page).to have_content "Você está visualizando o painel como #{user.name}."
     expect(page).to have_content user.name
 
     # Switch back
-    expect(page).to have_link "Voltar para #{admin_user.name}"
-    click_link "Voltar para #{admin_user.name}"
+    click_button "Voltar para #{admin_user.name}"
     expect(page).to have_content 'Você voltou ao painel administrativo.'
     expect(page).to have_content admin_user.name
   end
