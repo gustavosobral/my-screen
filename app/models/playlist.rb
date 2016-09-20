280227a53b7eaec831088a94ccdabb79902410fc
Gustavo Sobral <ghsobral@gmail.com>
2016-09-20 00:52:05 -0300

    Add Playlist model spec

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,9 @@ class Playlist < ActiveRecord::Base
   belongs_to :user
   has_many :terminals
   has_many :playlist_items, -> { order(:position) }, dependent: :destroy
+
+  validates :user,        presence: true
+  validates :title,       presence: true, length: { maximum: 45 }
+  validates :description, length: { maximum: 255 }
+  validates :duration,    numericality: { greater_than: 0 }
 end
