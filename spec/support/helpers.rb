9c99aa16d20eeabb307b9f1caa00963e0c6b28de
Gustavo Sobral <ghsobral@gmail.com>
2016-09-22 12:51:11 -0300

    Create click_button_by_action test helper

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -6,4 +6,8 @@ module Helpers
   def click_link_by_href(href)
     find(:xpath, "//a[@href='#{href}']").click
   end
+
+  def click_button_by_action(action)
+    find("form[action='#{action}']").click_button
+  end
 end
